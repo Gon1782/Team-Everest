e563df19638640a6cce07024a15acad9f40b7a05
Hoesu Ji <newasborn@naver.com>
2023-02-19 15:59:21 +0900

    [feat] 헤더 라우터 수정 및 레이아웃 컴포넌트로 통합

--- src/components/scrollToTopBtn/ScrollTopBtn.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ScrollTopBtn = () => {
-  return <div>ScrollTopBtn</div>;
-};
-
-export default ScrollTopBtn;
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,4 +1,3 @@
-import ScrollTopBtn from '@/components/scrollToTopBtn/ScrollTopBtn';
 import Header from '@/components/header/Header';
 import { Outlet } from 'react-router-dom';
 
@@ -6,7 +5,6 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <ScrollTopBtn />
       <Outlet />
       {/* children 대신 사용가능한 라이브러리, 상위 컴포넌트를 레이아웃화 해줌 */}
     </>
