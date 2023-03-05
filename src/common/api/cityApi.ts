e509423e565fb434709d450f70e7bc13c678301e
Gon <hss3522@gmail.com>
2023-03-06 01:49:14 +0900

    [Fix] try catch

--- src/common/api/cityApi.ts
+++ src/common/api/cityApi.ts
@@ -12,33 +12,39 @@ import {
 import { db } from './firebase';
 
 export const getCities = async (areaCode: string, sigunguCode: string) => {
-  const q = !!sigunguCode
-    ? query(
-        collection(db, 'cities'),
-        where('areaCode', '==', areaCode),
-        where('sigunguCode', '==', sigunguCode),
-      )
-    : query(collection(db, 'cities'), where('areaCode', '==', areaCode));
-  const data: City[] = [];
-  const get = await getDocs(q);
-  get.forEach((doc) => {
-    data.push({
-      name: doc.data().name,
-      areaCode: doc.data().areaCode,
-      sigunguCode: doc.data().sigunguCode,
-      reviewCount: doc.data().reviewCount,
-      engarea: doc.data().engarea,
-      description: doc.data().description,
-      hashtag: doc.data().hashtag,
-      tourcount: doc.data().tourcount,
-      tourdate: doc.data().tourdate,
-      spec: doc.data().spec,
-      mapx: doc.data().mapx,
-      mapy: doc.data().mapy,
-      image: doc.data().image,
+  try {
+    const q = !!sigunguCode
+      ? query(
+          collection(db, 'cities'),
+          where('areaCode', '==', areaCode),
+          where('sigunguCode', '==', sigunguCode),
+        )
+      : query(collection(db, 'cities'), where('areaCode', '==', areaCode));
+    const data: City[] = [];
+    const get = await getDocs(q);
+    get.forEach((doc) => {
+      data.push({
+        name: doc.data().name,
+        areaCode: doc.data().areaCode,
+        sigunguCode: doc.data().sigunguCode,
+        reviewCount: doc.data().reviewCount,
+        engarea: doc.data().engarea,
+        description: doc.data().description,
+        hashtag: doc.data().hashtag,
+        tourcount: doc.data().tourcount,
+        tourdate: doc.data().tourdate,
+        spec: doc.data().spec,
+        mapx: doc.data().mapx,
+        mapy: doc.data().mapy,
+        image: doc.data().image,
+      });
     });
-  });
-  return data[0]
+    return data[0];
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+  }
 };
 
 export const postCities = async (city: string, Data: Document) => {
