64ff59fd68dc2635d9fcb125dfdc6d016ce5fe53
freesian12 <zzte123@naver.com>
2023-03-06 21:14:44 +0900

    [design] 일정 페이지

---
+++ src/components/MyPlan/DateAndArea.tsx
@@ -0,0 +1,66 @@
+import {
+  Authority,
+  IsCalenderView,
+  NewPlanRecoil,
+  PlanType,
+} from '@/recoil/atom/MyPlan';
+import { BsFlagFill } from 'react-icons/bs';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { FaRegCalendarCheck } from 'react-icons/fa';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+const DateAndArea = () => {
+  const myPlan = useRecoilValue<PlanType | any>(NewPlanRecoil);
+  const setIsShowCalender = useSetRecoilState(IsCalenderView);
+  const authority = useRecoilValue(Authority);
+  return (
+    <PlanDate>
+      {authority.write ? (
+        <>
+          <img
+            src={'https://img.icons8.com/color/48/null/calendar--v1.png'}
+            onClick={() => setIsShowCalender((prev) => !prev)}
+            style={{ cursor: 'pointer', width: 30, height: 30 }}
+          />
+
+          <Date>
+            {myPlan?.startDate['yyyymmdd']} - {myPlan?.endDate['yyyymmdd']}
+          </Date>
+        </>
+      ) : (
+        // <>
+        //   <BsFlagFill size={30} color={'#E8F1F5'} />
+        //   <BsFlagFill size={30} color={'#DBE2EF'} />
+        //   <BsFlagFill size={30} color={'#004A7C'} />
+        //   <BsFlagFill size={30} color={'#FBE0C4'} />
+        //   <BsFlagFill size={30} color={'orange'} />
+        //   <BsFlagFill size={30} color={'#D8D2CB'} />
+
+        <Date>
+          {myPlan?.startDate['yyyymmdd']} - {myPlan?.endDate['yyyymmdd']}
+        </Date>
+
+        // </>
+      )}
+    </PlanDate>
+  );
+};
+
+export default DateAndArea;
+
+const PlanDate = styled.div`
+  display: block;
+  /* & > p {
+    font-size: 25px;
+  } */
+  text-align: center;
+  height: 80px;
+`;
+const Date = styled.p`
+  font-size: 22px;
+  color: gray;
+`;
+const showCalender = styled.button`
+  height: 25px;
+`;
---
+++ src/components/MyPlan/HandlerBtn.tsx
@@ -0,0 +1,190 @@
+import { Authority, PlanType } from '@/recoil/atom/MyPlan';
+import React from 'react';
+import { FaBookmark } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+import {
+  addPlan,
+  popPlan,
+  saveOtherPlan,
+  updatePlan,
+} from './MyPlannerHandler';
+
+const HandlerBtn = ({
+  userId,
+  uid,
+  plan,
+  planName,
+  planUniqueId,
+}: {
+  userId: string;
+  uid: string;
+  plan: PlanType;
+  planName: string;
+  planUniqueId: string;
+}) => {
+  const [authority, setAuthority] = useRecoilState(Authority);
+  const navigate = useNavigate();
+  const firstCheck = () => {
+    // 추가한 일정들이 있는지 확인
+    let eventCount = 0;
+    Object.values(plan.schedule).filter((item) => (eventCount += item.length));
+
+    if (!!!planName) {
+      alert('제목을 입력해주세요!');
+      return true;
+    }
+
+    if (!eventCount) {
+      alert('일정을 추가해주세요!');
+      return true;
+    }
+
+    return false;
+  };
+
+  const asking = (key: string) => {
+    if (window.confirm(`해당 일정을 ${key} 하시겠습니까?`)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const clickAddPlan = async (isShow: boolean) => {
+    if (firstCheck()) return;
+    try {
+      asking('저장')
+        ? await addPlan(plan, planName, uid, isShow, true).then(() => {
+            alert('완료 되었습니다!');
+            navigate('/my');
+          })
+        : alert('취소하셨습니다.');
+    } catch (e) {
+      alert('잠시 후에 시도해주세요');
+    }
+  };
+  const clickUpdatePlan = async (isShow: boolean) => {
+    if (firstCheck()) return;
+    try {
+      asking('수정')
+        ? await updatePlan(
+            plan,
+            planName,
+            uid,
+            planUniqueId,
+            isShow,
+            !!plan?.isMine,
+          ).then(() => {
+            alert('완료 되었습니다!');
+            navigate('/my');
+          })
+        : alert('취소하셨습니다.');
+    } catch (e) {
+      alert('잠시 후에 시도해주세요');
+    }
+  };
+  const clickBookMark = async () => {
+    try {
+      asking('북마크')
+        ? await saveOtherPlan(plan, planName, uid, userId, planUniqueId).then(
+            () => {
+              alert('완료 되었습니다!');
+              navigate('/my');
+            },
+          )
+        : alert('취소하셨습니다.');
+    } catch (e) {
+      alert('잠시 후에 시도해주세요');
+    }
+  };
+  const clickPopPlan = async () => {
+    try {
+      asking('삭제')
+        ? await popPlan(uid, planUniqueId).then(() => {
+            alert('완료 되었습니다!');
+            navigate('/my');
+          })
+        : alert('취소하셨습니다.');
+    } catch (e) {
+      alert('잠시 후에 시도해주세요');
+    }
+  };
+
+  return (
+    <>
+      {authority.write ? (
+        <>
+          <div></div>
+          {authority.update ? ( // 내 일정 수정할때
+            <div>
+              <PlanBtn color={'gray'} onClick={() => clickUpdatePlan(false)}>
+                임시저장
+              </PlanBtn>
+              <PlanBtn color={'gray'} onClick={() => clickUpdatePlan(true)}>
+                발행
+              </PlanBtn>
+            </div>
+          ) : (
+            // 내 일정 생성할때
+            <div>
+              <PlanBtn color={'gray'} onClick={() => clickAddPlan(false)}>
+                임시저장
+              </PlanBtn>
+              <PlanBtn color={'gray'} onClick={() => clickAddPlan(true)}>
+                발행
+              </PlanBtn>
+            </div>
+          )}
+        </>
+      ) : userId === uid ? (
+        // 내 일정 볼때
+        <div>
+          <PlanBtn
+            onClick={() =>
+              setAuthority({
+                write: true,
+                view: false,
+                update: true,
+              })
+            }
+            color={'dbe2ef'}
+          >
+            수정하기
+          </PlanBtn>
+          <PlanBtn onClick={() => clickPopPlan()} color={'dbe2ef'}>
+            삭제하기
+          </PlanBtn>
+        </div>
+      ) : (
+        // 다른 유저 일정 볼때
+        authority.view &&
+        userId !== uid && (
+          <div
+            style={{ display: 'flex', cursor: 'pointer' }}
+            onClick={() => clickBookMark()}
+          >
+            <FaBookmark color="red" />
+            일정 추가하기
+          </div>
+        )
+      )}
+    </>
+  );
+};
+
+export default HandlerBtn;
+
+export const PlanBtn = styled.button<{
+  backgroundColor?: string;
+  color?: string;
+}>`
+  width: 80px;
+  height: 28px;
+  font-size: 0.75rem;
+  /* background-color: #3f46ff;
+    color: #f3f3f3; */
+  background-color: #f1f6f9; //${(props) => props.color};
+  color: gray; //${(props) => props.color};
+  border-radius: 50px;
+`;
