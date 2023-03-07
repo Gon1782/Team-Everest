1030602c3d530a93a93f2a3677b07c486b88c771
Gon <hss3522@gmail.com>
2023-03-08 04:30:50 +0900

    [Fix] 이미지 드래그막기

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -152,6 +152,18 @@ li {
     font-style: normal;
 }
 
+img {
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+}
+
 
 
 `;
