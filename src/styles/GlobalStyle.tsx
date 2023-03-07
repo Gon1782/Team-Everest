692013b605ff5e4b552cd7b2caa89777e2254dee
Hoesu Ji <newasborn@naver.com>
2023-03-07 22:07:24 +0900

    [style] 폰트 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -16,12 +16,47 @@ html {
 body {
   font-family: 'Noto Sans KR', sans-serif;
   transition: all 0.3s;
+  color: #1d1d1d;
 }
 
 h1 {
- font-size: 3.125rem;
- font-weight: 900;
- font-family: 'S-CoreDream-7ExtraBold'
+  font-size: 3.125rem;
+  font-weight: 700;
+  font-family: 'S-CoreDream';
+}
+
+h2 {
+  font-size: 2.8rem;
+  font-weight: 600;
+  font-family: 'S-CoreDream';
+}
+
+h3 {
+  font-size: 2.6rem;
+  font-weight: 500;
+  font-family: 'S-CoreDream';
+}
+
+h4 {
+  font-size: 1.375rem;
+  font-weight: 400;
+  font-family: 'S-CoreDream';
+}
+
+h5 {
+  font-size: 1.25rem;
+  font-weight: 300;
+  font-family: 'S-CoreDream';
+}
+
+h6 {
+  font-size: 1.125rem;
+  font-weight: 200;
+  font-family: 'S-CoreDream';
+}
+
+p {
+  font-size: 1rem;
 }
 
 li {
@@ -30,21 +65,63 @@ li {
 
 
 
-
 @font-face {
-     font-family: 'S-CoreDream-3Light';
-     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
-     font-weight: normal;
-     font-style: normal;
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-1Thin.woff') format('woff');
+    font-weight: 100;
+    font-style: normal;
 }
 
 @font-face {
-     font-family: 'S-CoreDream-7ExtraBold';
-     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-7ExtraBold.woff') format('woff');
-     font-weight: normal;
-     font-style: normal;
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-2ExtraLight.woff') format('woff');
+    font-weight: 200;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-4Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
 }
 
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-5Medium.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-6Bold.woff') format('woff');
+    font-weight: 600;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-7ExtraBold.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-8Heavy.woff') format('woff');
+    font-weight: 800;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-9Black.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+}
 
 
 
